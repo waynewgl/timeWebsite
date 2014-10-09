4607b723edca7be12909d74ff69dfd73747a17fa
way <waynewgl@gmail.com>
2014-10-09 14:18:40 +0800

    first init

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,11 @@
+class MainController < ApplicationController
+  protect_from_forgery
+
+  def index
+
+
+
+  end
+
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimeCapsuleWebsite::Application.config.secret_token = '56723dc533318fcc3850de10981cbb23c8ad5936f7deb479841ec05d7b40211024caa4aadb9071be6e6cbbcc531fba0eef4801a142bf6adae0a524228a02f003'
